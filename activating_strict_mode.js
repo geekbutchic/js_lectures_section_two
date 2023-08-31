cb6db496ad38987b2b90d9560232455841641269
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2023-08-30 18:23:56 -0600

    adds notes on strict mode

---
+++ activating_strict_mode.js
@@ -0,0 +1,12 @@
+"use strict";
+// Activating strict mode - avoids accidental bugs
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log(`I can drive`);
+
+
+//const interface = 'Audio';
+// throws err unexpected reserve word
