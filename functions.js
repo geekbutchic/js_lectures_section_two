517f928640861cda2d54f2e1b32c59648d22500a
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2023-09-04 17:39:28 -0600

    adds solutions for lecture challenges

--- functions.js
+++ functions.js
@@ -32,3 +32,25 @@ In the string juice, the concatenation is happening inside the template literal 
 
 const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
+
+/* 
+Lecture : Functions Calling Other Functions.
+1.Write a function called 'describeCountry' which takes three parameters: 'country', 'population' and 'capitalCity'.  Based on this input, the functions returns a string with this format: 'Finland has 6 million people ans it's capital city is Helsinki'.
+2. Call this function 3 times, with input data for 3 different countries.  Store the returned values in 3 different variables, and log them to the console.
+*/
+
+function describeCountry(country, population, capitalCity) {
+  const description = `${country} has ${population} million people and it's capital city is ${capitalCity}.`;
+  return description;
+}
+
+const unitedStates = describeCountry(
+  "United States of America",
+  331.9,
+  "Washington D.C."
+);
+console.log(unitedStates);
+const spain = describeCountry("España", 47.42, "Madrid");
+console.log(spain);
+const italy = describeCountry("Italia", 59.11, "Roma");
+console.log(italy);
