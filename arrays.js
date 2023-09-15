77c2e91002453052d99f3803fa147478a14df89c
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2023-09-15 17:02:39 -0600

    adds solution to coding challenge in arrays.js file

--- arrays.js
+++ arrays.js
@@ -42,7 +42,7 @@ const calcAge = function(birthYear) {
 const years1 = [1990, 1967, 2002, 2010, 2018];
 
 const age = calcAge(years1[0]); // 33
-const age1 = calcAge(years1[1]); // 56 
+const age1 = calcAge(years1[1]); // 56
 const age2 = calcAge(years1[years1.length - 1]); //5
 console.log(age, age1, age2); // 33 56 5
 
@@ -53,3 +53,37 @@ const ages = [
   calcAge(years1[years1.length - 1])
 ];
 console.log(ages); // [ 33, 56, 5 ]
+
+/*
+Lecture : Introduction to Arrays
+1. Create an array containing 4 population values of 4 countries of your choice.  You may use the values you have been using previously. Store this array into a variable called 'populations'. 
+2. Log to the console whether the array has 4 elements or not (true or false). 
+3. Create an array called 'percentages' containing the percentages of the world population for these 4 population values. Use the function 'percentageOfWorld1' that you created earlier to compute the 4 percentage values.
+*/
+
+const populations = [331900000, 1444216107, 47420000, 10420000];
+
+const logValue = populations.length === 4;
+console.log(logValue);
+
+//The comparison operator === (strict equality) in JavaScript always returns either true or false based on whether the two operands being compared are equal or not.
+
+function percentageOfWorld1(population) {
+  const percentage = population / 7900000000 * 100;
+  return `${percentage.toFixed(2)}%`;
+}
+
+// Calculate the percentage for each population value and store them in an array
+
+// In this case, populations.map(...) is applying the percentageOfWorld1 function to each element in the populations array and creating a new array (percentages) with the calculated percentages for each population value.
+const percentages = populations.map(population =>
+  percentageOfWorld1(population)
+);
+
+console.log(Array.isArray(percentages), percentages); // Log the percentages array
+
+/* 
+When you use typeof to check the data type of an array in JavaScript, it will return "object". This is because, in JavaScript, arrays are a type of object. Arrays are a specialized type of object that has numeric indices and a length property, which makes them distinct from regular objects.
+
+So, when you use typeof on an array, you get "object" as the result because arrays are technically a subtype of objects. To specifically check if a variable is an array, you can use the Array.isArray() method, like this:
+*/
