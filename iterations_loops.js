efe620fae6a2e1d6e4ddb64819e983cd9a5d013d
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2023-09-28 19:26:57 -0600

    adds notes for iterations

---
+++ iterations_loops.js
@@ -0,0 +1,13 @@
+// Iterations / Loops
+
+//10 times etc...
+// console.log(`Lifting weights repetition 1 🏋🏻‍♀️`);
+// console.log(`Lifting weights repetition 2 🏋🏻‍♀️`);
+
+// For loops
+// 1. Initial value let rep = 1
+// 2. Condition - For loops keeps running while condition is true
+// 3. Update the counter similar to rep = rep + 1
+for (let rep = 1; rep <= 10; rep++) {
+  console.log(`Lifting weights repetition ${rep} 🏋🏻‍♀️`);
+}
