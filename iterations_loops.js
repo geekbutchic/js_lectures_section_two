765af2816eb775eec1a42a2a040a6f0c3103196f
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2023-09-28 19:44:22 -0600

    adds solution for iteration challenge

--- iterations_loops.js
+++ iterations_loops.js
@@ -11,3 +11,12 @@
 for (let rep = 1; rep <= 10; rep++) {
   console.log(`Lifting weights repetition ${rep} 🏋🏻‍♀️`);
 }
+
+/*
+Lecture : Iteration for Loop
+1. There are elections in your country! In a small town, there are only 50 voters.  Use a for loop to simulate the 50 people voting, by logging a string like this to the console.  `Voter number 1 is currently voting`.
+*/
+
+for (let voter = 1; voter <= 50; voter++) {
+    console.log(`Voter number ${voter} is currently voting`);
+}
